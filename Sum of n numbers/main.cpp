747487bc9ca437637463e62f6a111c368350e9c7
aifeelit <ifeelit416@gmail.com>
2021-10-01 00:31:11 +0530

    Sum of n numbers (#1)
    * Create main.cpp

---
+++ Sum of n numbers/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,sum;
+	cout<<"Enter n";
+	cin>>n;
+	sum = n*(n+1)/2;
+	cout<<"Sum - "<<sum;
+	// your code goes here
+	return 0;
+}
+
+
