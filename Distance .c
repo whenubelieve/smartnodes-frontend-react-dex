c325cc621312902ee9d22c565b7c529330945a3d
aifeelit <ifeelit416@gmail.com>
2021-10-22 21:06:11 +0530

    Update Distance .c

--- Distance .c
+++ Distance .c
@@ -1,8 +1,3 @@
-CR, [20.10.21 13:45]
-/*Program to read two input each representing the distances between two points in the Eucliean space,store these in structural variables and add the two distance values.
-Name: Sherin Alosious
-Class:ECE B
-Roll no:37*/
 #include<stdio.h>
 int main()
 {
