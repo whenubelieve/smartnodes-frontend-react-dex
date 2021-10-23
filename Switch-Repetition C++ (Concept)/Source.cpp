a854abae70410dd57d014670150e31fd0e26ceff
aifeelit <ifeelit416@gmail.com>
2021-10-23 11:28:20 +0500

    Add files via upload

---
+++ Switch-Repetition C++ (Concept)/Source.cpp
@@ -0,0 +1,41 @@
+// Source.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include"SwitchCase.h"
+#include"Repetitions.h"
+using namespace std;
+
+int main()
+{
+	//ex1_TernaryOperator();
+	//ex2_NestedTernaryOperator();
+	//ex3_SwitchCaseCalculator();
+	//ex4_DefaultCase();
+	//ex5_SwitchExpression();
+	//ex6_CaseValues();
+	//ex7_NestedSwitchCase();
+	
+	/*int a = 10, b = 10;
+	int RetValue = ex1_IncrementDecrement(a++, ++b);
+	cout << RetValue << endl;
+	cout << a << endl;*/
+
+	ex2_Counter();
+	//ex3_Table(5);
+	//ex3_Power(2);
+	//ex4_InfiniteLoop();
+	//ex5_InputValidation();
+	//ex6_SquarePattern(5);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
